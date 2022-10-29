73691e2fb80c8ee2b9b14a08c614359c18e4f28b
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-10-30 02:10:32 +0300

    sum

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+
+export const AppContainer = styled.div`
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  margin-left:850px;
+    `;
+
+    
---
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -0,0 +1,101 @@
+import PropTypes from 'prop-types';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Component } from 'react';
+import { PhoneForm } from './PhoneBook.styled';
+
+class PhoneBook extends Component {
+  state = {
+    name: '',
+    number: '',
+    isDisabled: false,
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const contact = {
+      //   id: shortid.generate(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+
+    this.props.addContact(contact);
+    this.reset();
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ isDisabled: false, [name]: value });
+    console.log(this.props.contacts);
+
+    const contactFinder = this.props.contacts.find(
+      contact =>
+        contact.name.toLowerCase() === value.toLowerCase() ||
+        contact.number === value
+    );
+    if (contactFinder) {
+      this.setState({ isDisabled: true });
+      Notify.warning(`${value} is already in contacts.`);
+      this.setState({ [name]: '' });
+    }
+  };
+
+  render() {
+    return (
+      <PhoneForm>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            placeholder="john doe"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}
+            isRequired
+          />
+        </label>
+        <label>
+          Number:
+          <input
+            type="tel"
+            name="number"
+            placeholder="+380 33 333 3333"
+            // className={styles.phoneInputCountry}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            value={this.state.number}
+            onChange={e => this.handleChange(e)}
+            isRequired
+          />
+        </label>
+
+        <button
+          className={PhoneBook.submitButton}
+          type="submit"
+          disabled={this.state.isDisabled}
+        >
+          add contact
+        </button>
+      </PhoneForm>
+    );
+  }
+}
+
+PhoneBook.propTypes = {
+  addContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default PhoneBook;
